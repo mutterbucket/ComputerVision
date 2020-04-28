f56676ff29356189c27482d0fed0abacbf2e3115
Ethan Triplett <43102357+mutterbucket@users.noreply.github.com>
2020-04-27 21:44:01 -0500

    Calculates distances between three hogs

--- ChallengeTriplett/ChallengeTriplett.py
+++ ChallengeTriplett/ChallengeTriplett.py
@@ -3,7 +3,7 @@ from skimage import io,data,exposure,color
 from skimage.feature import hog, canny
 import cv2
 
-base_address = "C:\\Users\\E\\source\\repos\\ComputerVision\\ChallengeTriplett\\"
+base_address = "C:\\Users\\a2tri\\source\\repos\\ComputerVision\\ChallengeTriplett\\"
 set1_address = base_address + "data_batch_1"
 set2_address = base_address + "data_batch_2"
 set3_address = base_address + "data_batch_3"
@@ -37,6 +37,12 @@ def oneD(img):
     flat = np.ravel(img)
     return flat
 
+def getHog(img):
+    img = reshape(img)
+    hog_image = wholeHog(img)
+    flat_hog = oneD(hog_image)
+    return flat_hog
+
 ####################################################################################
 ##                                     Main                                       ##
 ####################################################################################
@@ -49,11 +55,16 @@ set5 = unpickle(set5_address)
 testSet = unpickle(test_set_address)
 
 for x in range(100):
-    straight = list(set1.values())[2][x]
-    img = reshape(straight)
-    hog_image = wholeHog(img)
-    flat_hog = oneD(hog_image)
-    io.imsave(base_address + "\\hogs\\pic" + str(x) + "_norm.jpg", img)
-    io.imsave(base_address + "\\hogs\\pic" + str(x) + "_hog.jpg", hog_image)
+    hog1 = getHog(list(set1.values())[2][x])
+    hog2 = getHog(list(set1.values())[2][x+1])
+    hog3 = getHog(list(set1.values())[2][x+2])
+
+    dist1 = np.linalg.norm(hog1 - hog2)
+    dist2 = np.linalg.norm(hog1 - hog3)
+    dist3 = np.linalg.norm(hog2 - hog3)
+
+
+    #io.imsave(base_address + "\\hogs\\pic" + str(x) + "_norm.jpg", img)
+    #io.imsave(base_address + "\\hogs\\pic" + str(x) + "_hog.jpg", hog)
 
 print ("Done!")
